c44103fe20922f22bdfe693badf87fea77c8297d
Smilie Pop <arisatominato48@gmail.com>
2026-05-28 23:18:21 +0800

    Allow window to be dragged

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { Window } from "./components/Window";
 
 function App() {
     return (
-        <div className="w-screen h-screen bg-[url('/img/nso_bg_1080.png')] relative flex items-center justify-center">
-            <Window title="Test">
-                <div className="flex flex-row items-center justify-center flex-1">
-                    Test
-                </div>
-            </Window>
+        <div className="w-screen h-screen bg-[url('/img/nso_bg_1080.png')] flex flex-col items-center justify-center">
+            <div className="flex flex-1" id="desktop">
+                <Window title="Test">
+                    <div className="flex flex-row items-center justify-center flex-1">
+                        Test
+                    </div>
+                </Window>
+            </div>
+            <div className="h-4 w-full bg-amber-600" id="taskbar"></div>
         </div>
     );
 }
--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,3 +1,6 @@
+import Draggable from "react-draggable";
+import { useRef } from "react";
+
 interface WindowProps {
     title: string;
     children?: React.ReactNode;
@@ -6,26 +9,38 @@ interface WindowProps {
 export const Window = (prop: WindowProps) => {
     const { title, children } = prop;
 
+    const nodeRef = useRef(null);
+
     return (
-        <div className="absolute border-2 border-nso-purple bg-nso-cyan px-1 pt-1 box-border flex flex-col shadow-[4px_4px_#4d23cf55] min-w-64 min-h-64">
-            <div className="flex flex-row items-center bg-nso-light-pink border-2 border-nso-purple box-border font-nso-dinkie-9px align-middle text-nso-purple text-[16px]/[0.5em] p-1 gap-1">
-                <div className="bg-nso-purple min-h-4 min-w-4" />
-                <span className="w-full align-middle">{title}</span>
-                <div className="flex gap-1">
+        <Draggable nodeRef={nodeRef} handle="#title-bar">
+            <div
+                className="absolute border-2 border-nso-purple bg-nso-cyan px-1 pt-1 box-border flex flex-col shadow-[4px_4px_#4d23cf55] min-w-64 min-h-64"
+                ref={nodeRef}
+            >
+                <div
+                    className="flex flex-row items-center bg-nso-light-pink border-2 border-nso-purple box-border p-1 gap-1"
+                    id="title-bar"
+                >
                     <div className="bg-nso-purple min-h-4 min-w-4" />
-                    <div className="bg-nso-purple min-h-4 min-w-4" />
-                    <div className="bg-nso-purple min-h-4 min-w-4" />
+                    <span className="w-full text-[16px]/[16px] font-nso-dinkie-9px text-nso-purple">
+                        {title}
+                    </span>
+                    <div className="flex gap-1">
+                        <div className="border-2 border-nso-purple min-h-4 min-w-4" />
+                        <div className="border-2 border-nso-purple min-h-4 min-w-4" />
+                        <div className="border-2 border-nso-purple min-h-4 min-w-4" />
+                    </div>
+                </div>
+                <div className="bg-white border-2 border-nso-purple my-1 border-box flex flex-col flex-1">
+                    {children}
+                </div>
+                <div className="flex flex-row">
+                    <div className="h-2 w-8 bg-nso-dark-pink border-2 border-b-0 border-box border-nso-purple" />
+                    <div className="h-1.5 w-1.5 bg-nso-cyan border-2 border-nso-purple border-box ml-0.5" />
+                    <div className="h-1.5 w-1.5 bg-nso-cyan border-2 border-nso-purple border-box ml-0.5" />
+                    <div className="h-1.5 w-1.5 bg-nso-cyan border-2 border-nso-purple border-box ml-0.5" />
                 </div>
             </div>
-            <div className="bg-white border-2 border-nso-purple my-1 border-box flex flex-col flex-1">
-                {children}
-            </div>
-            <div className="flex flex-row">
-                <div className="h-2 w-8 bg-nso-dark-pink border-2 border-b-0 border-box border-nso-purple" />
-                <div className="h-1.5 w-1.5 bg-nso-cyan border-2 border-nso-purple border-box ml-0.5" />
-                <div className="h-1.5 w-1.5 bg-nso-cyan border-2 border-nso-purple border-box ml-0.5" />
-                <div className="h-1.5 w-1.5 bg-nso-cyan border-2 border-nso-purple border-box ml-0.5" />
-            </div>
-        </div>
+        </Draggable>
     );
 };
